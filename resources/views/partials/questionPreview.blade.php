3736ec30183f1f92a3635dbd23203e5ecaae80ce
Eduardo <edurocadas@gmail.com>
2023-11-17 18:17:10 +0000

    finished questionPreview with css

--- resources/views/partials/questionPreview.blade.php
+++ resources/views/partials/questionPreview.blade.php
@@ -1,15 +1,20 @@
-<a href="/questions/{{ $question->id }}">
-    <article class="question-prev" data-id="{{$question->id}}">
-        <header>
-            <h2>{{$question->title}}</h2> 
-            <h2>{{$question->user()->username}}</h2>  
+<article class="question-prev card border-primary mb-3"  data-id="{{$question->id}}" style="max-width: 20rem;">
+    <a href="/questions/{{ $question->id }}">
+        <header class="card-header">
+            <h3>{{$question->user()->username}}</h3>
             <h3>{{$question->firstVersion()->date}}</h3>
         </header>
-        <div class="votes">
-            <!-- todo: get question score-->
+        <div class="card-body">
+            <h4 class="card-title">{{$question->title}}</h4>
+            <div class="votes">
+                {{$question->votes()}}
+            <div>
+            <p class="card-text">
+                {{$question->updatedVersion()->body}}
+            </p>
         <div>
-        <p>
-            {{$question->updatedVersion()->body}}
-        </p>
-    </article>
     <a>
+</article>
+
+
+
