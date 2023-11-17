c80e0456896e72c8a0edfd812a36f066ddfd53d6
up202108752 <up202108752@fe.up.pt>
2023-11-17 23:59:23 +0000

    Enhanced form design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('login') }}">
-    {{ csrf_field() }}
+<div class="col align-middle">
+    <div class="row justify-content-center p-3">
+        <form method="POST" action="{{ route('login') }}" class="col-4 border border-primary border-2 rounded p-3">
+            {{ csrf_field() }}
 
-    <fieldset>
-        <legend>Register</legend>
+            <fieldset>
+                <legend>Login</legend>
 
-        <div class="form-group">
-            <label for="email" class="form-label mt-4">E-Mail address</label>
-            <input id="email" type="email" name="email" class="form-control" placeholder="Enter email" value="{{ old('email') }}" required>
-            @if ($errors->has('email'))
-                <span class="error">
-                    {{ $errors->first('email') }}
-                </span>
-            @endif        
-        </div>
+                <div class="form-group">
+                    <label for="email" class="form-label mt-4">E-Mail address</label>
+                    <input id="email" type="email" name="email" class="form-control" placeholder="Enter email" value="{{ old('email') }}" required>
+                    @if ($errors->has('email'))
+                        <span class="error">
+                            {{ $errors->first('email') }}
+                        </span>
+                    @endif        
+                </div>
 
-        <div class="form-group">
-            <label for="password" class="form-label mt-4">Password</label>
-            <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
-            @if ($errors->has('password'))
-                <span class="error">
-                    {{ $errors->first('password') }}
-                </span>
-            @endif
-        </div>
+                <div class="form-group">
+                    <label for="password" class="form-label mt-4">Password</label>
+                    <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+                    @if ($errors->has('password'))
+                        <span class="error">
+                            {{ $errors->first('password') }}
+                        </span>
+                    @endif
+                </div>
 
-        <div class="form-check">
-            <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" value="" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
-            </label>
-        </div>
+                <div class="form-check">
+                    <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input" value="" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember me
+                    </label>
+                </div>
 
-        <button type="submit" class="btn btn-primary">
-            Login
-        </button>
-        <a class="btn btn-secondary" href="{{ route('register') }}">Register</a>
-        @if (session('success'))
-            <p class="success">
-                {{ session('success') }}
-            </p>
-        @endif 
-    </fieldset>
-</form>
+                <div class="form-group pt-3">
+                    <button type="submit" class="btn btn-primary">
+                        Login
+                    </button>
+                    <a class="btn btn-secondary" href="{{ route('register') }}">Register</a>
+                    @if (session('success'))
+                        <p class="success">
+                            {{ session('success') }}
+                        </p>
+                    @endif 
+                </div>
+                
+            </fieldset>
+        </form>
+    </div>
+</div>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,62 +1,69 @@
 @extends('layouts.app')
 
 @section('content')
-<form method="POST" action="{{ route('register') }}">
-    {{ csrf_field() }}
+<div class="col align-middle">
+    <div class="row justify-content-center p-3">
+        <form method="POST" action="{{ route('register') }}" class="col-4 border border-primary border-2 rounded p-3">
+            {{ csrf_field() }}
 
-    <fieldset>
-      <legend>Register</legend>
+            <fieldset>
+            <legend>Register</legend>
 
-      <div class="form-group">
-        <label for="name" class="form-label mt-4">Name</label>
-        <input id="name" type="text" name="name" class="form-control" placeholder="Insert your name (optional)" value="{{ old('name') }}" autofocus>
-      </div>
-      @if ($errors->has('name'))
-        <span class="error">
-            {{ $errors->first('name') }}
-        </span>
-      @endif
+            <div class="form-group">
+                <label for="name" class="form-label mt-4">Name</label>
+                <input id="name" type="text" name="name" class="form-control" placeholder="Insert your name (optional)" value="{{ old('name') }}" autofocus>
+            </div>
+            @if ($errors->has('name'))
+                <span class="error">
+                    {{ $errors->first('name') }}
+                </span>
+            @endif
 
-      <div class="form-group">
-        <label for="username" class="form-label mt-4">Username</label>
-        <input id="username" type="text" name="username" class="form-control" placeholder="Insert username" value="{{ old('username') }}" required>
-      </div>
-      @if ($errors->has('username'))
-        <span class="error">
-            {{ $errors->first('username') }}
-        </span>
-      @endif
+            <div class="form-group">
+                <label for="username" class="form-label mt-4">Username</label>
+                <input id="username" type="text" name="username" class="form-control" placeholder="Insert username" value="{{ old('username') }}" required>
+            </div>
+            @if ($errors->has('username'))
+                <span class="error">
+                    {{ $errors->first('username') }}
+                </span>
+            @endif
 
-      <div class="form-group">
-        <label for="email" class="form-label mt-4">E-Mail address</label>
-        <input id="email" type="email" name="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" value="{{ old('email') }}" required>
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        @if ($errors->has('email'))
-          <span class="error">
-              {{ $errors->first('email') }}
-          </span>
-        @endif        
-      </div>
-      
-      <div class="form-group">
-        <label for="password" class="form-label mt-4">Password</label>
-        <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
-        @if ($errors->has('password'))
-          <span class="error">
-              {{ $errors->first('password') }}
-          </span>
-        @endif
-      </div>
+            <div class="form-group">
+                <label for="email" class="form-label mt-4">E-Mail address</label>
+                <input id="email" type="email" name="email" class="form-control" aria-describedby="emailHelp" placeholder="Enter email" value="{{ old('email') }}" required>
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                @if ($errors->has('email'))
+                <span class="error">
+                    {{ $errors->first('email') }}
+                </span>
+                @endif        
+            </div>
+            
+            <div class="form-group">
+                <label for="password" class="form-label mt-4">Password</label>
+                <input id="password" type="password" name="password" class="form-control" placeholder="Password" required>
+                @if ($errors->has('password'))
+                <span class="error">
+                    {{ $errors->first('password') }}
+                </span>
+                @endif
+            </div>
 
-      <div class="form-group">
-        <label for="password-confirm" class="form-label mt-4">Confirm Password</label>
-        <input id="password-confirm" type="password" name="password_confirmation" class="form-control" placeholder="Confirm password" required>
-      </div>
+            <div class="form-group">
+                <label for="password-confirm" class="form-label mt-4">Confirm Password</label>
+                <input id="password-confirm" type="password" name="password_confirmation" class="form-control" placeholder="Confirm password" required>
+            </div>
+            
+            <div class="form-group pt-3">
+                <button type="submit" class="btn btn-primary">
+                Register
+                </button>
+                <a class="btn btn-secondary" href="{{ route('login') }}">Login</a>
+            </div>
 
-      <button type="submit" class="btn btn-primary">
-        Register
-      </button>
-      <a class="btn btn-secondary" href="{{ route('login') }}">Login</a>
-    </fieldset>
-</form>
+            </fieldset>
+        </form>
+    </div>
+</div>
 @endsection
