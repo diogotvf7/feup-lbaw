ea1d515112984fadcb9def202731952c037f126c
up202108752 <up202108752@fe.up.pt>
2023-11-19 19:13:22 +0000

    Corrected routings on login/register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,7 +19,7 @@ class LoginController extends Controller
     public function showLoginForm()
     {
         if (Auth::check()) {
-            return redirect('/welcome');
+            return redirect()->route('topQuestions');
         } else {
             return view('auth.login');
         }
@@ -38,7 +38,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials, $request->filled('remember'))) {
             $request->session()->regenerate();
  
-            return redirect()->intended('/welcome');
+            return redirect()->route('topQuestions');
         }
  
         return back()->withErrors([
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,7 @@
                             <input class="form-control me-sm-2" type="search" placeholder="Search">
                             <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
                         </form>
+                        <a class="btn btn-secondary ms-3" href="{{ url('/login') }}">Login / Register</a>
                     </div>
                 </div>
             </nav>
