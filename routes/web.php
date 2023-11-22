8c42628a3a84bfb20c67fc4234442b6146e9c406
Rita Lopes <up202108819@up.pt>
2023-11-22 23:51:15 +0000

    Finished openapi

--- routes/web.php
+++ routes/web.php
@@ -33,19 +33,8 @@ Route::controller(QuestionController::class)->group(function () {
     Route::post('/questions/store', 'store')->name('question.store');
     Route::get('/questions/search', 'search')->name('search');
     Route::get('/questions/{question}', 'show');
-});
-
-Route::controller(UserController::class)->group(function () {
-    Route::get('/admin/users', 'index')->name('users')->middleware(AdminMiddleware::class);
-    Route::patch('/admin/users/{user}/promote', 'promote')->name('user.promote');
-    Route::patch('/admn/users/{user}/demote', 'demote')->name('user.demote');
-    Route::get('/admin/users/{user}/edit', 'edit')->name('admin.users.edit')->middleware(AdminMiddleware::class);;
-
-    //Route::get('/users/{user}', 'show')->where('user', '[0-9]+')->name('users.profile');
-    //Route::delete('/users/{user}/delete', 'destroy')->name('users.destroy');
-    //Route::patch('/users/{user}/update', 'update')->name('users.update');
-    //Route::get('/user/create', 'create')->name('user.create')->middleware(AdminMiddleware::class);;
-    Route::post('/user/store', 'store')->name('user.store');
+    Route::patch('/questions/edit', 'edit')->name('question/edit');
+    Route::delete('/questions/delete', 'destroy')->name('question/delete');
 });
 
 Route::controller(AnswerController::class)->group(function () {
@@ -54,24 +43,32 @@ Route::controller(AnswerController::class)->group(function () {
     Route::delete('/answers/delete', 'destroy')->name('answer/delete');
 });
 
-Route::controller(QuestionController::class)->group(function () {
-    Route::patch('/questions/edit', 'edit')->name('question/edit');
-    Route::delete('/questions/delete', 'destroy')->name('question/delete');
+Route::controller(UserController::class)->group(function () {
+    Route::get('/admin/users', 'index')->name('users')->middleware(AdminMiddleware::class);
+    Route::patch('/admin/users/{user}/promote', 'promote')->name('user.promote');
+    Route::patch('/admn/users/{user}/demote', 'demote')->name('user.demote');
+    Route::get('/admin/users/{user}/edit', 'edit')->name('admin.users.edit')->middleware(AdminMiddleware::class);;
+
+    Route::get('/users/{user}', 'show')->where('user', '[0-9]+')->name('users.profile');
+    Route::delete('/users/{user}/delete', 'destroy')->name('users.destroy');
+    Route::patch('/users/{user}/update', 'update')->name('users.update');
+    Route::get('/user/create', 'create')->name('user.create')->middleware(AdminMiddleware::class);;
+    Route::post('/user/store', 'store')->name('user.store');
 });
 
-// API
+//API
 Route::controller(QuestionController::class)->group(function () {
     Route::get('/api/questions', 'fetch');
 });
 
 // Authentication
 Route::controller(LoginController::class)->group(function () {
-   // Route::get('/login', 'showLoginForm')->name('login');
-    //Route::post('/login', 'authenticate');
-    //Route::post('/logout', 'logout')->name('logout');
+    Route::get('/login', 'showLoginForm')->name('login');
+    Route::post('/login', 'authenticate');
+    Route::post('/logout', 'logout')->name('logout');
 });
 
 Route::controller(RegisterController::class)->group(function () {
-   // Route::get('/register', 'showRegistrationForm')->name('register');
-    //Route::post('/register', 'register');
+    Route::get('/register', 'showRegistrationForm')->name('register');
+    Route::post('/register', 'register');
 });
