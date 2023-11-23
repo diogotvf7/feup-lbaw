032b100b735767a27fa3cad99961f707df3a43c4
up202108752 <up202108752@fe.up.pt>
2023-11-23 00:47:16 +0000

    Fix

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -4,11 +4,6 @@
 <section>
     <section id="search-results" class="p-5">
         <h2 class="text-primary-emphasis">Search results for '<strong>{{$query}}</strong>'</h2>
-
-
-<section>
-    <section id="search-results" class="p-5">
-        <h2 class="text-primary-emphasis">Search results for '<strong>{{$query}}</strong>'</h2>
         <div class="accordion mt-3 rounded border border-primary" id="accordionSearch">
             <div class="accordion-item list-group accordion-header d-flex justify-content-between align-items-center" id="headingOne">
                 <button class="list-group-item accordion-button d-flex flex-row justify-content-between align-items-center" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
