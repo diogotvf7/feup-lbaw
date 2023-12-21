3547971a61cdfd6226efcf8d3515bebc0bbce539
diogotvf7 <diogotvf7@gmail.com>
2023-12-21 23:17:55 +0000

    Bugfix v.overflow

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ import './scroll-top.js';
 import enableTagModal from './add-tags.js';
 import enableUserModal from './add-user.js';
 import loadAnswers from './answers-loader.js';
+import editComment from './comment-edit.js';
 import enableFollowTag from './follow-tag.js';
 import enableNotifications, {markQuestionNotifRead, notificationButton} from './notifications.js';
 import editQuestion from './question-edit.js';
