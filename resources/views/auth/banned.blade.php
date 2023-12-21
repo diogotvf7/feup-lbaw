364466b1903eb6305b91ee2fe1c8bc542ad5d99c
Eduardo <edurocadas@gmail.com>
2023-12-21 21:25:17 +0000

    fixes

--- resources/views/auth/banned.blade.php
+++ resources/views/auth/banned.blade.php
@@ -6,7 +6,7 @@
 
       <div class="d-flex flex-column" id = message style="padding:5vh;">
         <h1 style="color: red;"> This User Is Currently Banned</h1>
-        <h3 style="margin:1vh;">This account has been banned by Geras moderation, you are no longer able to create or interact with content using this account, with no way to undo this decision.</h3>
+        <h3 style="margin:1vh;">This account has been banned by Geras' moderation, you are no longer able to create or interact with content using this account, with no way to undo this decision.</h3>
     <div>
 
 </div>
